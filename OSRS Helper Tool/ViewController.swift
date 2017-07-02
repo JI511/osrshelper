92fabcb7908e62a80470b2880b388918be38fd35
John Ingwersen <johni@iastate.edu>
2017-07-02 13:02:43 -0500

    Initial Commit

---
+++ OSRS Helper Tool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSRS Helper Tool
+//
+//  Created by John Ingwersen on 7/2/17.
+//  Copyright © 2017 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
